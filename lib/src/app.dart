fc7957125b535388b3044884db3bf677b562638c
Chatnarint Boonsaeng <53619535+whyzotee@users.noreply.github.com>
2025-11-27 19:56:20 +0700

    chore: Initialize project

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'ui/home.dart';
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const HomePage(),
+    );
+  }
+}
